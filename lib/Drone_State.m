17bdef00b3b233e853db2324203195a44c458137
기민재 <heartnebula@naver.com>
2025-10-28 17:57:38 +0900

    make drone simulator with PID controller

---
+++ lib/Drone_State.m
@@ -0,0 +1,87 @@
+classdef Drone_State < handle
+
+%% MEMBERS
+    properties
+        g
+        t
+        dt
+        tf
+
+        m
+        l
+        I
+
+        x           % [X, Y, Z, dX, dY, dZ, phi, theta, psi, p, q, r]'
+        r           % [X, Y, Z]'
+        dr          % [dX, dY, dZ]'
+        euler       % [pihi, theta, psi]'
+        w           % [p, q, r]'
+
+        dx
+
+        T           % T_sum
+        M           % [M1, M2, M3]'
+    end
+
+%% METHODS
+    methods
+        %% CONSTRUCTOR
+        function obj = Drone_State(params, initStates, simTime, dt)
+            obj.g = 9.81;
+            obj.t = 0.0;
+            obj.dt = dt;
+            obj.tf = simTime;
+
+            obj.m = params('mass');
+            obj.l = params('armLength');
+            obj.I = [params('Ixx'), 0, 0; ...
+                     0, params('Iyy'), 0; ...
+                     0, 0, params('Izz')];
+
+            obj.x = initStates;
+            obj.r = obj.x(1:3);
+            obj.dr = obj.x(4:6);
+            obj.euler = obj.x(7:9);
+            obj.w = obj.x(10:12);
+
+            obj.dx = zeros(12,1);
+            obj.T = 0;
+            obj.M = [0;0;0]; % initial M
+
+        end
+
+        function state = GetState(obj)
+            state = obj.x;
+        end
+
+        function obj = EvalEOM(obj)
+            bRi = RPY2Rot(obj.euler);
+            R = bRi';
+
+            obj.dx(1:3) = obj.dr; %x(4:6)
+            obj.dx(4:6) = 1 / obj.m * ([0; 0; obj.m*obj.g] + R * obj.T * [0; 0; -1]);
+
+            phi = obj.euler(1); theta = obj.euler(2);
+            obj.dx(7:9) = [ 1 sin(phi)*tan(theta) cos(phi)*tan(theta);
+                            0 cos(phi)            -sin(phi);
+                            0 sin(phi)*sec(theta) cos(phi)*sec(theta)] * obj.w;
+
+            obj.dx(10:12) = (obj.I)\(obj.M - cross(obj.w, obj.I * obj.w));
+        end
+
+        function obj = UpdateState(obj, u)
+            obj.T = u(1);
+            obj.M = u(2:4);
+            
+            obj.t = obj.t + obj.dt;
+
+            obj.EvalEOM();
+            obj.x = obj.x + obj.dx.*obj.dt; % 룬지쿠타 사용해도 됨. 오일러 방식임
+
+            obj.r = obj.x(1:3);
+            obj.dr = obj.x(4:6);
+            obj.euler = obj.x(7:9);
+            obj.w = obj.x(10:12); % sensor disturbance 추가시 rand 사용해 sensor 구성
+        end
+    end
+end
---
+++ main.m
@@ -0,0 +1,162 @@
+% main_refactored.m
+clc;
+clear;
+close all
+addpath('./lib');
+%% DEFINE
+R2D = 180/pi;
+D2R = pi/180;
+%% Simulation time
+simulationTime = 2;
+dt = 0.01;
+
+%% INITIAL PARAMS
+% can initialize parameter, when using PID control, you must initialize
+% Gain K.
+drone1_params = containers.Map({'mass', 'armLength', 'Ixx', 'Iyy', 'Izz'}, ...
+    {1.25, 0.265, 0.0232, 0.0232, 0.0468});
+drone1_initStates = [0, 0, -6, ...       % X, Y, Z
+    0, 0, 0, ...                        % dX, dY, dZ
+    0, 0, 0, ...                        % phi, theta, psi
+    0, 0, 0]';                          % p, q, r
+
+drone1_body = [ 0.265,      0,     0, 1; ...
+                    0, -0.265,     0, 1; ...
+               -0.265,      0,     0, 1; ...
+                    0,  0.265,     0, 1; ...
+                    0,      0,     0, 1; ...
+                    0,      0, -0.15, 1]';
+
+%PID Gain(optional when you use PID controller)
+drone1_gains = containers.Map(...
+    {'P_phi', 'I_phi', 'D_phi', ...
+    'P_theta', 'I_theta', 'D_theta', ...
+    'P_psi', 'I_psi', 'D_psi', ...
+    'P_zdot', 'I_zdot', 'D_zdot'}, ...
+    {0.2, 0.0, 0.15, ...
+    0.2, 0.0, 0.15, ...
+    0.2, 0.0, 0.15, ...
+    10.0, 0.05, 0.0});
+
+%% 객체 생성(초기화)
+% 1. import drone dynamics
+drone1 = Drone_State(drone1_params, drone1_initStates, simulationTime, dt);
+% 2. import drone controller
+controller1 = Control_PID(drone1_gains, drone1_params, dt);
+
+%% command signal
+commandSig(1) = 10.0 * D2R; % phi
+commandSig(2) = 10.0 * D2R; % theta
+commandSig(3) = 10.0 * D2R; % psi
+commandSig(4) = -1.0; % z_dot
+
+%% Figure
+fig1 = figure('pos', [0 200 800 800]);
+h = gca;
+view(3);
+fig1.CurrentAxes.ZDir = 'Reverse';
+fig1.CurrentAxes.YDir = 'Reverse';
+
+axis equal;
+grid on;
+
+xlim([-5 5]); ylim([-5 5]); zlim([-8 0]);
+xlabel('X[m]'); ylabel('Y[m]'); zlabel('Z[m]');
+
+hold(gca, 'on');
+drone1_state = drone1.GetState();
+wHb = [RPY2Rot(drone1_state(7:9))' drone1_state(1:3);
+       0 0 0 1];
+
+drone1_world = wHb * drone1_body;
+drone1_atti = drone1_world(1:3, :);
+
+fig1_ARM13 = plot3(gca, drone1_atti(1,[1 3]), drone1_atti(2, [1 3]), drone1_atti(3, [1 3]), '-ro', 'MarkerSize', 5);
+fig1_ARM24 = plot3(gca, drone1_atti(1,[2 4]), drone1_atti(2, [2 4]), drone1_atti(3, [2 4]), '-bo', 'MarkerSize', 5);
+fig1_payload = plot3(gca, drone1_atti(1,[5 6]), drone1_atti(2, [5 6]), drone1_atti(3, [5 6]), '-k', 'Linewidth', 3);
+fig1_shadow = plot3(gca, 0, 0, 0, 'xk', 'LineWidth', 3);
+
+hold(gca, "off");
+
+%% Init. Data Fig.
+fig2 = figure('pos', [800 550 800 450]);
+subplot(2,3,1)
+title('phi[deg]');
+grid on;
+hold on;
+subplot(2,3,2)
+title('theta[deg]');
+grid on;
+hold on;
+subplot(2,3,3)
+title('psi[deg]');
+grid on;
+hold on;
+subplot(2,3,4)
+title('x[m]');
+grid on;
+hold on;
+subplot(2,3,5)
+title('y[m]');
+grid on;
+hold on;
+subplot(2,3,6)
+title('zdot[m/s]');
+grid on;
+hold on;
+
+%% SIMULATION LOOP
+for i = 1:simulationTime/dt
+    drone1_state = drone1.GetState();
+    u = controller1.AttitudeCtrl(drone1_state, commandSig);
+    drone1.UpdateState(u);
+    drone1_state = drone1.GetState();
+
+    %% 3D Plot
+    wHb = [RPY2Rot(drone1_state(7:9))' drone1_state(1:3); 0 0 0 1];
+    drone1_world = wHb * drone1_body;
+    drone1_atti = drone1_world(1:3, :);
+    set(fig1_ARM13, ...
+        'xData', drone1_atti(1,[1 3]), ...
+        'yData', drone1_atti(2,[1 3]), ...
+        'zData', drone1_atti(3,[1 3]));
+
+    set(fig1_ARM24, ...
+        'xData', drone1_atti(1,[2 4]), ...
+        'yData', drone1_atti(2,[2 4]), ...
+        'zData', drone1_atti(3,[2 4]));
+
+    set(fig1_payload, ...
+        'xData', drone1_atti(1,[5 6]), ...
+        'yData', drone1_atti(2,[5 6]), ...
+        'zData', drone1_atti(3,[5 6]));
+
+    set(fig1_shadow, ...
+        'xData', drone1_state(1), ...
+        'yData', drone1_state(2), ...
+        'zData', 0);
+
+    drawnow;
+    
+    %% Data Plot
+    figure(2)
+    subplot(2,3,1)
+    plot(i/100, drone1_state(7)*R2D, '.');
+    subplot(2,3,2)
+    plot(i/100, drone1_state(8)*R2D, '.');
+    subplot(2,3,3)
+    plot(i/100, drone1_state(9)*R2D, '.');
+    subplot(2,3,4)
+    plot(i/100, drone1_state(1), '.');
+    subplot(2,3,5)
+    plot(i/100, drone1_state(2), '.');
+    subplot(2,3,6)
+    plot(i/100, drone1_state(6), '.');
+
+    drawnow;
+    
+    if (drone1_state(3) >= 0)
+        msgbox('Crashed!!', 'Error', 'error');
+        break;
+    end
+end
